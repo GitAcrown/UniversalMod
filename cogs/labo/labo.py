9836193080a9d7df30e7c40f2b3814072947d934
GitAcrown <Wellington>
2018-05-11 21:48:48 +0200

    Labo | Commandes randoms

--- cogs/labo/labo.py
+++ cogs/labo/labo.py
@@ -7,7 +7,7 @@ from sumy.nlp.stemmers import Stemmer
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.parsers.html import HtmlParser
 from sumy.parsers.plaintext import PlaintextParser
-from sumy.summarizers.lex_rank import LexRankSummarizer as Summarizer
+from sumy.summarizers.lsa import LsaSummarizer as Summarizer
 from sumy.utils import get_stop_words
 
 from .utils.dataIO import fileIO, dataIO
