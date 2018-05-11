62d20cb40521ea3ea376f6291a41050d72979c6c
GitAcrown <Wellington>
2018-05-11 18:32:07 +0200

    Labo | Commandes randoms

---
+++ cogs/labo/labo.py
@@ -0,0 +1,46 @@
+import os
+
+from discord.ext import commands
+
+from .utils.dataIO import fileIO, dataIO
+
+
+class Labo:
+    """Module poubelle d'expérimentation et commandes random - parfois des trucs sympa en sortent !"""
+    def __init__(self, bot):
+        self.bot = bot
+        self.sys = dataIO.load_json("data/labo/sys.json")  # Pas très utile mais on le garde pour plus tard
+        self.sys_def = {}
+
+    @commands.command(pass_context=True)
+    async def vaporwave(self, ctx, *texte):
+        """Formatte un texte en ｖａｐｏｒｗａｖｅ"""
+        norm = [l for l in "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 "]
+        vapo = [l for l in "ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ" \
+                           "１２３４５６７８９０　"]
+        texte = " ".join(texte)
+        fin_texte = texte
+        for char in texte:
+            if char in norm:
+                ind = norm.index(char)
+                fin_texte = fin_texte.replace(vapo[ind])
+        await self.bot.say("**Ｖａｐｏｒ** | {}".format(fin_texte))
+
+
+def check_folders():
+    if not os.path.exists("data/labo"):
+        print("Creation du fichier Labo ...")
+        os.makedirs("data/labo")
+
+
+def check_files():
+    if not os.path.isfile("data/labo/sys.json"):
+        print("Création de labo/sys.json ...")
+        fileIO("data/labo/sys.json", "save", {})
+
+
+def setup(bot):
+    check_folders()
+    check_files()
+    n = Labo(bot)
+    bot.add_cog(n)
