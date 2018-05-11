3e4527eb5a61499d896d414ebbe42f12007e4bdb
GitAcrown <Wellington>
2018-05-11 21:08:43 +0200

    Labo | Commandes randoms

--- cogs/labo/labo.py
+++ cogs/labo/labo.py
@@ -10,7 +10,6 @@ from sumy.parsers.html import HtmlParser
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.summarizers.lsa import LsaSummarizer as Summarizer
 from sumy.utils import get_stop_words
-import numpy
 
 from .utils.dataIO import fileIO, dataIO
 
