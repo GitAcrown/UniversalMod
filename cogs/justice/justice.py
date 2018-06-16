cb863a3c8e496a26b5489ce4b02465affa821ff9
GitAcrown <Acrown@outlook.fr>
2018-06-16 22:43:04 +0200

    Tech | Meta

--- cogs/justice/justice.py
+++ cogs/justice/justice.py
@@ -28,123 +28,6 @@ class Justice:
         fileIO("data/justice/reg.json", "save", self.reg)
         return True
 
-    """@commands.command(aliases=["p", "jail"], pass_context=True)
-    @checks.admin_or_permissions(manage_messages=True)
-    async def prison(self, ctx, user: discord.Member, temps: str = "5m"):
-        Emprisonner un membre sur un salon dédié (Défaut: 5m)
-
-        <user> = Utilisateur à mettre en prison
-        <temps> = Temps pendant lequel l'utilisateur est en prison (Minimum 60s)
-        Format de temps:
-        's' pour seconde(s)
-        'm' pour minute(s)
-        'h' pour heure(s)
-        'j' pour jour(s)
-        Exemple : &p @membre 5h
-
-        Note: Il est possible d'ajouter ou retirer du temps avec '+' et '-' avant le chiffre
-        Exemple : &p @membre +10m
-        reg = r"<@(.\d+)>\s?([+-])?([0-9])?([smhj])?"
-        server = ctx.message.server
-        if temps.isdigit():
-            await self.bot.whisper("**N'oubliez pas le format !**\n"
-                                   "Formats disponibles : m (minutes), h (heures), j (jours)\n"
-                                   "Exemple: &p @membre 5h")
-            return
-        role = self.sys["ROLE_PRISON"]
-        apply = discord.utils.get(ctx.message.server.roles, name=role)
-        form = temps[-1:]
-        if form not in ["s", "m", "h", "j"]:
-            await self.bot.say("Ce format n'existe pas (s, m, h ou j)")
-            return
-        save = user.name
-        if temps.startswith("+") or temps.startswith("-"):  # Ajouter ou retirer du temps de prison
-            val = temps.replace(form, "")
-            val = int(val.replace(temps[0], ""))
-            if user.id in self.reg:
-                if role in [r.name for r in user.roles]:
-                    modif = self.convert_sec(form, val)
-                    if temps[0] == "+":
-                        self.reg[user.id]["FIN_PEINE"] += modif
-                        self.new_event("add", user.id, ctx.message.author.id, modif)
-                        estim = time.strftime("%H:%M", time.localtime(self.reg[user.id]["FIN_PEINE"]))
-                        await self.bot.say(
-                            "**Ajout de *{}{}* pour *{}* réalisé avec succès.**".format(val, form, user.name))
-                        await self.bot.send_message(user,
-                                                    "*{}{}* ont été ajoutés à ta peine par *{}*\nSortie désormais prévue à: `{}`".format(
-                                                        val, form, ctx.message.author.name, estim))
-                    elif temps[0] == "-":
-                        self.reg[user.id]["FIN_PEINE"] -= modif
-                        self.new_event("sub", user.id, ctx.message.author.id, modif)
-                        estim = time.strftime("%H:%M", time.localtime(self.reg[user.id]["FIN_PEINE"]))
-                        await self.bot.say(
-                            "**Retrait de *{}{}* pour *{}* réalisé avec succès.**".format(val, form, user.name))
-                        await self.bot.send_message(user,
-                                                    "*{}{}* ont été retirés de ta peine par *{}*\nSortie désormais prévue à: `{}`".format(
-                                                        val, form, ctx.message.author.name, estim))
-                    else:
-                        await self.bot.say("Symbole non reconnu. Ajoutez du temps avec '+' et retirez-en avec '-'.")
-                        return
-                else:
-                    await self.bot.say(
-                        "L'utilisateur n'est pas en prison. Vous ne pouvez donc pas lui ajouter ni retirer du temps de peine.")
-                    return
-            else:
-                await self.bot.say(
-                    "L'utilisateur n'a jamais été en prison. Vous ne pouvez donc pas lui ajouter ni retirer du temps de peine.")
-                return
-        else:
-            val = int(temps.replace(form, ""))
-            sec = self.convert_sec(form, val)
-            if sec >= 60:  # Au moins une minute
-                if sec > 86400:
-                    await self.bot.whisper(
-                        "Il est déconseillé d'utiliser la prison pour une peine dépassant 24h (1j) à cause des instabilités de Discord pouvant causer une peine infinie.")
-                if user.id not in self.reg:
-                    self.reg[user.id] = {"FIN_PEINE": None,
-                                         "DEB_PEINE": None,
-                                         "BANG": 0,
-                                         "ROLES": [r.name for r in user.roles],
-                                         "D_PSEUDO": user.display_name,
-                                         "TRACKER": []}
-                    self.save()
-                if role not in [r.name for r in user.roles]:
-                    b_peine = time.time()
-                    estim = time.strftime("%H:%M", time.localtime(b_peine + sec))
-                    self.reg[user.id]["DEB_PEINE"] = b_peine
-                    self.reg[user.id]["FIN_PEINE"] = b_peine + sec
-                    self.new_event("in", user.id, ctx.message.author.id, temps)
-                    await self.bot.add_roles(user, apply)
-                    await self.bot.say("{} **a été mis(e) en prison pendant *{}* par *{}***".format(user.mention, temps,
-                                                                                                    ctx.message.author.name))
-                    await self.bot.send_message(user,
-                                                "**Tu as été mis(e) en prison pendant {}**\nSortie prévue à: `{}`\n"
-                                                "Un salon textuel écrit est disponible sur le serveur afin de contester cette punition ou afin d'obtenir plus d'informations.".format(
-                                                    temps, estim))
-                    self.save()
-                    while time.time() < self.reg[user.id]["FIN_PEINE"]:
-                        await asyncio.sleep(0.5)
-                    if user in server.members:
-                        if role in [r.name for r in user.roles]:
-                            self.reg[user.id]["DEB_PEINE"] = self.reg[user.id]["FIN_PEINE"] = 0
-                            self.new_event("out", user.id, "auto")
-                            await self.bot.remove_roles(user, apply)
-                            await self.bot.say("{} **est libre**".format(user.mention))
-                            self.save()
-                        else:
-                            return
-                    else:
-                        await self.bot.say(
-                            "{} ne peut être sorti de prison car il n'est plus sur le serveur.".format(save))
-                else:
-                    self.reg[user.id]["DEB_PEINE"] = self.reg[user.id]["FIN_PEINE"] = 0
-                    self.new_event("out", user.id, ctx.message.author.id)
-                    await self.bot.remove_roles(user, apply)
-                    await self.bot.say("{} **a été libéré par *{}***".format(user.mention, ctx.message.author.name))
-                    self.save()
-            else:
-                await self.bot.say("Le temps minimum est de 1m")"""
-
     def convert_sec(self, form: str, val: int):
         if form == "j":
             return val * 86400
@@ -480,6 +363,7 @@ class Justice:
         Types:
         > Entrée
         r> Retour en prison
+        V> Visite de la prison
         < Sortie
         x< Erreur sortie
         !< Sortie par modérateur
@@ -508,8 +392,10 @@ class Justice:
             await self.bot.say("**Vide** | La prison n'a enregistré aucune action sur ce serveur.")
 
     @commands.command(aliases=["pl"], pass_context=True)
-    async def prisonliste(self, ctx):
-        """Liste les membres en prison"""
+    async def prisonliste(self, ctx, bot_slmt: bool = True):
+        """Liste les membres en prison
+
+        <bot_slmt> = Si FALSE, affiche les membres qui n'ont pas été emprisonnés via le bot"""
         server = ctx.message.server
         if server.id not in self.sys:
             self.sys[server.id] = self.base_sys
@@ -525,11 +411,16 @@ class Justice:
                 al.append(u)
                 user = server.get_member(u)
                 estim = time.strftime("%H:%M", time.localtime(self.reg[server.id][user.id]["TS_SORTIE"]))
-                txt += "{} ─ Sortie à `{}`\n".format(user.mention, estim)
-        for member in server.members:
-            if self.sys[server.id]["PRISON_ROLE"] in [r.name for r in member.roles]:
-                if member.id not in al:
-                    txt += "{} ─ Heure de sortie inconnue\n".format(member.mention)
+                if self.reg[server.id][u]["VISITE"]:
+                    vtxt = " (En visite)"
+                else:
+                    vtxt = ""
+                txt += "{} ─ Sortie à `{}`{}\n".format(user.mention, estim, vtxt)
+        if not bot_slmt:
+            for member in server.members:
+                if self.sys[server.id]["PRISON_ROLE"] in [r.name for r in member.roles]:
+                    if member.id not in al:
+                        txt += "{} ─ Heure de sortie inconnue\n".format(member.mention)
         if txt == "":
             await self.bot.say("**Vide** | Aucun membre n'est emprisonné en ce moment même.")
             return
@@ -653,7 +544,8 @@ class Justice:
             if user.id not in self.reg[server.id]:
                 self.reg[server.id][user.id] = {"TS_ENTREE": 0,
                                                 "TS_SORTIE": 0,
-                                                "CASIER": []}
+                                                "CASIER": [],
+                                                "VISITE": False}
             if role not in [r.name for r in user.roles]:
                 b_peine = time.time()
                 self.reg[server.id][user.id]["TS_ENTREE"] = b_peine
@@ -740,6 +632,129 @@ class Justice:
                         await self.bot.send_message(prisonchan, embed=em)
                 await asyncio.sleep(7)
                 await self.bot.delete_message(notif)
+
+    @commands.command(aliases=["vp"], pass_context=True)
+    @checks.admin_or_permissions(manage_roles=True)
+    async def visite(self, ctx, user: discord.Member = None):
+        """Permet de visiter la prison pendant 10 minutes
+
+        [user] = Membre à emprisonner pour la visite si ce n'est pas vous-même"""
+        message = ctx.message
+        server = message.server
+        temps = "10m"
+        form = temps[-1:]  # C'est le format du temps (smhj)
+        if form not in ["s", "m", "h", "j"]:
+            await self.bot.whisper("**Unités** | `m` = Minutes, `h` = Heures, `j` = Jours\n"
+                                   "Exemple: `{}p @membre 5h`".format(ctx.prefix))
+            return
+        if server.id not in self.sys:
+            self.sys[server.id] = self.base_sys
+        if server.id not in self.reg:
+            self.reg[server.id] = {}
+        if "APPEL_SALON" not in self.sys[server.id]:
+            self.sys[server.id]["APPEL_SALON"] = False
+            self.sys[server.id]["APPEL_USE"] = []
+        appel = True if self.sys[server.id]["APPEL_SALON"] else False
+        role = self.sys[server.id]["PRISON_ROLE"]
+        prisonchan = self.bot.get_channel(self.sys[server.id]["PRISON_SALON"]).name if \
+            self.sys[server.id]["PRISON_SALON"] else False
+        try:
+            apply = discord.utils.get(message.server.roles, name=role)  # Objet: Rôle
+        except:
+            await self.bot.say("**Erreur** | Le rôle *{}* n'est pas présent sur ce serveur.".format(role))
+            return
+
+        val = int(temps.replace(form, ""))
+        sec = self.convert_sec(form, val)
+        n = 0
+        if user.id not in self.reg[server.id]:
+            self.reg[server.id][user.id] = {"TS_ENTREE": 0,
+                                            "TS_SORTIE": 0,
+                                            "CASIER": [],
+                                            "VISITE": False}
+        if "VISITE" not in self.reg[server.id][user.id]:
+            self.reg[server.id][user.id]["VISITE"] = False
+        if role not in [r.name for r in user.roles]:
+            b_peine = time.time()
+            self.reg[server.id][user.id]["VISITE"] = True
+            self.reg[server.id][user.id]["TS_ENTREE"] = b_peine
+            self.reg[server.id][user.id]["TS_SORTIE"] = b_peine + sec
+            estim = time.strftime("%H:%M", time.localtime(self.reg[server.id][user.id]["TS_SORTIE"]))
+            await self.bot.add_roles(user, apply)
+            if appel and user.id not in self.sys[server.id]["APPEL_USE"]:
+                self.sys[server.id]["APPEL_USE"].append(user.id)
+            msg = "{} ─ Visite de la prison (10 minutes)".format(user.mention)
+            estim_txt = "Sortie estimée à {}".format(estim)
+            self.add_event(user, "V>", "{}{}".format(val, form), author=ctx.message.author)
+            em = discord.Embed(description="**Visite de la prison** ─ 10 minutes", color=apply.color)
+            em.set_footer(text="Sortie prévue à {}".format(estim))
+            try:
+                await self.bot.send_message(user, embed=em)
+            except:
+                if prisonchan:
+                    await self.bot.send_message(prisonchan, "{}".format(user.mention))
+                    await self.bot.send_message(prisonchan, embed=em)
+
+            em = discord.Embed(description=msg, color=apply.color)
+            em.set_footer(text=estim_txt)
+            notif = await self.bot.say(embed=em)
+            await asyncio.sleep(6)
+            await self.bot.delete_message(notif)
+
+            self.save()
+            while time.time() < self.reg[server.id][user.id]["TS_SORTIE"]:
+                await asyncio.sleep(1)  # TIMER ============#
+
+            if user in server.members:
+                if role in [r.name for r in user.roles]:
+                    self.reg[server.id][user.id]["TS_ENTREE"] = self.reg[server.id][user.id]["TS_SORTIE"] = 0
+                    self.reg[server.id][user.id]["VISITE"] = False
+                    self.add_event(user, "<")
+                    await self.bot.remove_roles(user, apply)
+                    if user.id in self.sys[server.id]["APPEL_USE"] and appel:
+                        self.sys[server.id]["APPEL_USE"].remove(user.id)
+                    em = discord.Embed(description="**Visite de la prison** ─ Votre visite est terminée !",
+                                       color=apply.color)
+                    try:
+                        await self.bot.send_message(user, embed=em)
+                    except:
+                        if prisonchan:
+                            await self.bot.send_message(prisonchan, "{}".format(user.mention))
+                            await self.bot.send_message(prisonchan, embed=em)
+                    rand = random.choice(["a terminé sa visite", "regagne sa liberté"])
+                    em = discord.Embed(description="{} {}".format(user.mention, rand), color=apply.color)
+                    notif = await self.bot.say(embed=em)
+                    await asyncio.sleep(6)
+                    await self.bot.delete_message(notif)
+                else:
+                    return
+            else:
+                em = discord.Embed(description="**Fin de visite de ** <@{}> | Ce membre n'est plus sur le serveur.")
+                if user.id in self.sys[server.id]["APPEL_USE"] and appel:
+                    self.sys[server.id]["APPEL_USE"].remove(user.id)
+                self.add_event(user, "x<")
+                notif = await self.bot.say(embed=em)
+                await asyncio.sleep(5)
+                await self.bot.delete_message(notif)
+        else:
+            self.reg[server.id][user.id]["TS_ENTREE"] = self.reg[server.id][user.id]["TS_SORTIE"] = 0
+            self.reg[server.id][user.id]["VISITE"] = False
+            await self.bot.remove_roles(user, apply)
+            if user.id in self.sys[server.id]["APPEL_USE"] and appel:
+                self.sys[server.id]["APPEL_USE"].remove(user.id)
+            self.add_event(user, "!<", author=ctx.message.author)
+            em = discord.Embed(description="{} a été sorti.e par {}".format(
+                user.mention, ctx.message.author.mention), color=apply.color)
+            notif = await self.bot.say(embed=em)
+            em = discord.Embed(description="**Visite de la prison** ─ On vous a sorti plus tôt", color=apply.color)
+            try:
+                await self.bot.send_message(user, embed=em)
+            except:
+                if prisonchan:
+                    await self.bot.send_message(prisonchan, "{}".format(user.mention))
+                    await self.bot.send_message(prisonchan, embed=em)
+            await asyncio.sleep(7)
+            await self.bot.delete_message(notif)
 
     async def renew(self, user):
         server = user.server
